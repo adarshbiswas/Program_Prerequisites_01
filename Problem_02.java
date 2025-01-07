bedad537d1f7dae22fd8882bbbeff88e3599f184
adarsh biswas <officiallyadarsh2003@gmail.com>
2025-01-07 21:39:53 +0530

    Problem_02 solved

---
+++ Problem_02.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Problem_02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the first number, a : ");
+        int a = sc.nextInt();
+        System.out.print("Enter the second number, b : ");
+        int b = sc.nextInt();
+        int sum = a + b;
+        System.out.println("The sum of two numbers is: " + sum );
+        
+    }
+}
